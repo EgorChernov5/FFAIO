cbf8c5549b70ad51e2d0bc62fb089be64695c6a3
EgorChernov5 <krish19poroh@mail.ru>
2025-11-27 21:32:34 +0300

    Update get_weights and backward_pass

--- src/models/abc_model.py
+++ src/models/abc_model.py
@@ -84,38 +84,20 @@ class ABCModel(ABC):
     def get_weights_layers(self) -> list[ABCLayer]:
         return [struc_element for struc_element in self.arch_model if isinstance(struc_element, ABCLayer)]
     
-    def get_weights(self) -> np.ndarray:
-        return np.array([layer.get_weights() for layer in self.get_weights_layers()])
+    def get_weights(self) -> tuple[np.ndarray, np.ndarray | None]:
+        W, b = [], []
+        for layer in self.get_weights_layers():
+            weights, bias = layer.get_weights()
+            W.append(weights)
+            if bias is not None: b.append(bias)
+        
+        return np.array(W), np.array(b) if len(b) else None
     
     def backward_pass(self, loss: ABCLoss):
-        # Инициализируем переменную хранящую слой, который стоит правее в прямом проходе
-        next_layer = None
-        for layer in reversed(self.arch_model):
-            # Выходной слой
-            if next_layer is None:
-                # Считаем частные производные Loss функции по выходам последнего слоя
-                delta = loss.pd_wrt_inputs(layer.outputs)
-            else:  # Скрытый слой
-                # Структурный слой
-                if not isinstance(layer, ABCLayer):
-                    # Считаем дельта правило
-                    delta = delta*layer.pd_wrt_inputs()
-                else:  # Слой весов
-                    # Частная производная струтурного слоя по выходу слоя весов [n_samples, n_inputs]
-                    dF_dI = next_layer.pd_wrt_inputs()
-                    # Считаем частные производные слоя весов по весам [n_samples, n_inputs]
-                    dZ_dW = layer.pd_wrt_w()
-                    # Локальная ошибка для этого слоя [n_samples, n_outputs]
-                    local_error = delta*dF_dI
-                    # Частная производная Loss по весу нейрона: dL_dW = dL_dA*dF_dI*dZ_dW
-                    gradients = local_error.T@dZ_dW / len(layer.inputs)
-
-                    # Сохраняем градиент для весов
-                    layer.gradients = gradients
-                    # Передаём ошибку дальше влево через веса
-                    delta = local_error@layer.pd_wrt_inputs()
-
-            next_layer = layer
+        delta = loss.backward_pass(self.arch_model[-1].outputs)
+        for layer in reversed(self.arch_model[:-1]):         
+            # Считаем дельта правило или градиент весов и передаём ошибку дальше влево
+            delta = layer.backward_pass(delta)
 
     def train_model(
             self,
