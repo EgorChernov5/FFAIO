63a8a2cffecedf165ef1ef2467968069f4d41812
EgorChernov5 <krish19poroh@mail.ru>
2025-12-09 18:22:07 +0300

    Add get_features

--- src/models/cnn_model.py
+++ src/models/cnn_model.py
@@ -1,9 +1,20 @@
+import numpy as np
+
 from src.models import ABCModel
 from src.layers import ABCLayer
 from src.activations import ABCActivation
-from src.structure_layers import ABCStructureLayer
+from src.structure_layers import ABCStructureLayer, FlattenLayer
 
 
 class CNN(ABCModel):
     def __init__(self, arch_model: list[ABCLayer| ABCActivation | ABCStructureLayer]):
         super().__init__(arch_model)
+
+    def get_features(self, X: np.ndarray) -> np.ndarray:
+        outputs = X
+        for layer in self.arch_model:
+            outputs = layer(outputs)
+            if isinstance(layer, FlattenLayer):
+                break
+
+        return outputs
